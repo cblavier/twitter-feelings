01a79bef9b8cce5cd7de235bd28c49479df3294d
Christian Blavier <cblavier@gmail.com>
2015-03-04 23:03:23 +0100

    order does not matter for CLI options

--- lib/twitter_feelings.ex
+++ lib/twitter_feelings.ex
@@ -3,7 +3,7 @@ defmodule TwitterFeelings do
   alias TwitterFeelings.CorpusBuilder.Builder,    as: Builder
   alias TwitterFeelings.CorpusBuilder.TweetStore, as: TweetStore
 
-  @default_query_count 5000
+  @default_tweet_count 500_000
   @default_lang        :en
 
   def main(argv) do
@@ -14,28 +14,31 @@ defmodule TwitterFeelings do
   end
 
   defp parse_args(argv) do
-    parse = OptionParser.parse(argv,
-      switches: [ help: :boolean, lang: :string, mood: :string, query_count: :integer],
+    {options, args, _} = OptionParser.parse(argv,
+      switches: [ help: :boolean, lang: :string, mood: :string, tweet_count: :integer],
       aliases:  [ h: :help ]
     )
-    case parse do
-    { [ help: true ] } -> :help
-    { [ lang: lang, mood: "positive", query_count: count ], ["build-corpus"], _ } -> { lang, :positive, count }
-    { [ lang: lang, mood: "negative", query_count: count ], ["build-corpus"], _ } -> { lang, :negative, count }
-    { [ lang: lang, mood: "positive" ], ["build-corpus"], _ } -> { lang, :positive, @default_query_count }
-    { [ lang: lang, mood: "negative" ], ["build-corpus"], _ } -> { lang, :negative, @default_query_count }
-    _ -> :help
+    options_map = Enum.into(options, %{})
+    case args do
+      ["build-corpus"] ->
+        case options_map do
+          %{ lang: lang, mood: "positive", tweet_count: count } -> { lang, :positive, count }
+          %{ lang: lang, mood: "negative", tweet_count: count } -> { lang, :negative, count }
+          %{ lang: lang, mood: "positive" } -> { lang, :positive, @default_tweet_count }
+          %{ lang: lang, mood: "negative" } -> { lang, :negative, @default_tweet_count }
+        end
+      _ -> :help
     end
   end
 
-  defp process({lang, mood, query_count}) do
+  defp process({lang, mood, tweet_count}) do
     TweetStore.set_lang_and_mood(lang, mood)
-    Builder.build_corpus(String.to_atom(lang), mood, query_count)
+    Builder.build_corpus(String.to_atom(lang), mood, tweet_count)
   end
 
   defp process(:help) do
     IO.puts """
-    usage: twitter_feelings build-corpus --query-count [ count | #{@default_query_count} ] --mood [ positive | negative ] --lang [ language | #{@default_lang}]
+    usage: twitter_feelings build-corpus --lang [ language ] --mood [ positive | negative ] --tweet-count [ count ]
     """
     System.halt(0)
   end
