e02878e9f2e3a80525c24a9029e5982a0715aaf6
Christian Blavier <cblavier@gmail.com>
2015-03-05 09:31:38 +0100

    added a testcase with no_more_max_id

--- test/twitter_feelings/corpus_builder/builder_test.exs
+++ test/twitter_feelings/corpus_builder/builder_test.exs
@@ -20,7 +20,7 @@ defmodule TwitterFeelings.CorpusBuilder.BuilderTest do
   end
 
   test "processes and store tweets" do
-    time = x_msecs_from_now(200)
+    time = x_msecs_from_now(700)
     tweet_count = fn ->
       change_behavior_after(time,
         fn -> 1 end,
@@ -29,6 +29,18 @@ defmodule TwitterFeelings.CorpusBuilder.BuilderTest do
     end
     with_mock TSearch, [search: fn(_,_,_) -> {:ok, statuses, max_id} end] do
       with_mock TStore, [store_tweet: fn(_) -> end, tweet_count: tweet_count] do
+        Builder.build_corpus(:fr, :positive, 2)
+        :timer.sleep(10)
+        assert called TStore.store_tweet("thee namaste nerdz #freebandnames")
+        assert called TStore.store_tweet("mexican heaven the hell #freebandnames")
+        assert called TStore.store_tweet("the foolish mortals #freebandnames")
+      end
+    end
+  end
+
+  test "it stops when there is no longer max_id" do
+    with_mock TSearch, [search: fn(_,_,_) -> {:ok, statuses, :no_more_max_id} end] do
+      with_mock TStore, [store_tweet: fn(_) -> end, tweet_count: fn -> 1 end] do
         Builder.build_corpus(:fr, :positive, 2)
         :timer.sleep(10)
         assert called TStore.store_tweet("thee namaste nerdz #freebandnames")
@@ -49,6 +61,5 @@ defmodule TwitterFeelings.CorpusBuilder.BuilderTest do
   def max_id do
     "249279667666817023"
   end
-
 
 end
